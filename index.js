52c75ee3b18b3eecddc05681a92f3109f1291968
Aurora Nou <auroranou@gmail.com>
2017-08-11 08:45:24 -0400

    comments, use mousedown and up instead of click

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 
 let context = new AudioContext();
 
+// this generates a wave
 let osc = context.createOscillator();
+// sine is the default, but just explicitly stating here
+osc.type = 'sine';
 
+// gain in an analog system is how much extra electricity you boost the signal with
 let amp = context.createGain();
 
-
+// grab all of the key elements and attached listeners
 let keys = document.querySelectorAll('.key');
 
-keys.forEach((k) => k.addEventListener('click', onKeyPress));
+keys.forEach((k) => {
+  k.addEventListener('mousedown', playNote);
+  k.addEventListener('mouseup', releaseNote);
+});
 
-function onKeyPress(e) {
+function playNote(e) {
   let note = e.target.id;
   console.log(note);
 }
 
+function releaseNote(e) {
+  let note = e.target.id;
+  console.log(note);
+}
