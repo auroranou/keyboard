1275f9b6165ca3ae8179036fd69da8bb69687ffe
Aurora Nou <auroranou@gmail.com>
2017-08-10 21:58:28 -0400

    bind event listeners to keys

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ let context = new AudioContext();
 let osc = context.createOscillator();
 
 let amp = context.createGain();
+
+
+let keys = document.querySelectorAll('.key');
+
+keys.forEach((k) => k.addEventListener('click', onKeyPress));
+
+function onKeyPress(e) {
+  let note = e.target.id;
+  console.log(note);
+}
+
