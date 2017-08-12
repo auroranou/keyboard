53d68b6b409ea82eff2a33146d4415ac222de8b8
ian29 <ian.villeda@gmail.com>
2017-08-12 15:09:37 -0400

    passing notes generator

--- test/notes.test.js
+++ test/notes.test.js
@@ -1,5 +1,5 @@
 const test = require('tape');
-const notes = require('./lib/notes.js');
+const notes = require('../lib/notes.js');
 
 test('[notes] correct pitch', t => {
   t.equals(notes(3), 523.3, 'c3 = 523.3');
