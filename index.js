ed3a9fa262e28a326deb263a3230dd25e52f8df3
Aurora Nou <auroranou@gmail.com>
2017-08-12 14:28:18 -0400

    i guess we can make it a js class or whatever

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 'use strict';
 
-let ctx = new AudioContext();
-
-// this generates a wave
-let osc = ctx.createOscillator();
-
-// gain in an analog system is how much extra electricity you boost the signal with
-let amp = ctx.createGain();
-
-let keyFreqMap = {
+const keyFreqMap = {
   c4: 261.6,
   'c4#': 277.2,
   d4: 293.7,
@@ -24,50 +16,66 @@ let keyFreqMap = {
   c5: 523.3
 };
 
-function init() {
-  // sine is the default, but just explicitly stating here
-  osc.type = 'sine';
-  osc.start(0);
+class Keyboard {
+  constructor() {
+    this.ctx = new AudioContext();
 
-  amp.gain.value = 0;
+    // this generates a wave
+    this.osc = this.ctx.createOscillator();
 
-  osc.connect(amp);
-  amp.connect(ctx.destination);
-}
+    // sine is the default, but just explicitly stating here
+    this.osc.type = 'sine';
+    this.osc.start(0);
 
-// grab all of the key elements and attached listeners
-let keys = document.querySelectorAll('.key');
-let keyDiv = document.getElementById('key-name');
+    // gain in an analog system is how much extra electricity you boost the signal with
+    this.amp = this.ctx.createGain();
 
-keys.forEach((k) => {
-  k.addEventListener('mousedown', playNote);
-  k.addEventListener('mouseup', releaseNote);
-});
+    this.amp.gain.value = 0;
 
-function playNote(e) {
-  let note = e.target.id;
-  let freq = keyFreqMap[note];
-  osc.frequency.value = freq;
-  amp.gain.value = 1;
+    // connect the things
+    this.osc.connect(this.amp);
+    this.amp.connect(this.ctx.destination);
 
-  getKeyName(note);
-}
-
-function releaseNote(e) {
-  amp.gain.value = 0;
-}
-
-function getKeyName(str) {
-  let values = str.split(/[0-9]/g);
-  let keyName = values[0].toUpperCase();
-  if (values[1]) {
-    let mod = values[1] === '#'
-      ? '-sharp'
-      : '-flat';
-    keyName += mod;
+    this.bindEvents();
   }
 
-  keyDiv.innerHTML = keyName;
+  bindEvents() {
+    // grab all of the key elements and attach listeners
+    let keys = document.querySelectorAll('.key');
+
+    keys.forEach((k) => {
+      k.addEventListener('mousedown', this.playNote.bind(this));
+      k.addEventListener('mouseup', this.releaseNote.bind(this));
+    });
+  }
+
+  playNote(e) {
+    let note = e.target.id;
+    let freq = keyFreqMap[note];
+    console.log(note, freq);
+    this.osc.frequency.value = freq;
+    this.amp.gain.value = 1;
+
+    this.setKeyName(note);
+  }
+
+  releaseNote(e) {
+    this.amp.gain.value = 0;
+  }
+
+  setKeyName(str) {
+    let values = str.split(/[0-9]/g);
+    let keyName = values[0].toUpperCase();
+    if (values[1]) {
+      let mod = values[1] === '#'
+        ? '-sharp'
+        : '-flat';
+      keyName += mod;
+    }
+
+    let keyDiv = document.getElementById('key-name');
+    keyDiv.innerHTML = keyName;
+  }
 }
 
-init();
+let keyboard = new Keyboard();
