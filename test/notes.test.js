1671bda0a68a8ac7f4b53c1d4b991e7f04ea9b15
ian29 <ian.villeda@gmail.com>
2017-08-12 16:27:36 -0400

    getSteps: ugly but it works

--- test/notes.test.js
+++ test/notes.test.js
@@ -1,7 +1,17 @@
 const test = require('tape');
 const notes = require('../lib/notes.js');
 
-test('[notes] correct pitch', t => {
-  t.equals(notes(3), 523.3, 'c3 = 523.3');
+test('[notes] getFreq', t => {
+  t.equals(notes.getFreq(3), 523.3, 'c3 = 523.3');
+  t.end();
+});
+
+test('[notes] getSteps', t => {
+  t.equals(notes.getSteps('c', 3), -22, 'c3 = -22');
+  t.equals(notes.getSteps('c', 4), -9, 'c4 = -9');
+  t.equals(notes.getSteps('a', 4), 0, 'a4 = 0');
+  t.equals(notes.getSteps('a#', 4), 1, 'a#4 = 1');
+  t.equals(notes.getSteps('c', 5), 3, 'c5 = 3');
+  t.equals(notes.getSteps('c', 6), 15, 'c6 = 15');
   t.end();
 });
