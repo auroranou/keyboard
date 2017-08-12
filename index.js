b95e9d0511b59e9c991e482546a814d1613b0ea8
Aurora Nou <auroranou@gmail.com>
2017-08-11 22:40:05 -0400

    assign frequencies to mid-c octave

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 'use strict';
 
-let context = new AudioContext();
+let ctx = new AudioContext();
 
 // this generates a wave
-let osc = context.createOscillator();
+let osc = ctx.createOscillator();
 // sine is the default, but just explicitly stating here
 osc.type = 'sine';
 
 // gain in an analog system is how much extra electricity you boost the signal with
-let amp = context.createGain();
+let amp = ctx.createGain();
+
+let keyFreqMap = {
+  c4: 261.6,
+  'c4#': 277.2,
+  d4: 293.7,
+  e4b: 311.1,
+  e4: 329.6,
+  f4: 349.2,
+  'f4#': 370.0,
+  g4: 392.0,
+  'g4#': 415.3,
+  a4: 440.0,
+  b4b: 466.2,
+  b4: 493.9,
+  c5: 523.3
+};
+
+function init() {
+  osc.start(0);
+
+  amp.gain.value = 0;
+
+  osc.connect(amp);
+  amp.connect(ctx.destination);
+}
+
+init();
 
 // grab all of the key elements and attached listeners
 let keys = document.querySelectorAll('.key');
@@ -19,11 +46,11 @@ keys.forEach((k) => {
 });
 
 function playNote(e) {
-  let note = e.target.id;
-  console.log(note);
+  let freq = keyFreqMap[e.target.id];
+  osc.frequency.value = freq;
+  amp.gain.value = 1;
 }
 
 function releaseNote(e) {
-  let note = e.target.id;
-  console.log(note);
+  amp.gain.value = 0;
 }
